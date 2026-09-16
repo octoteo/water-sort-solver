a2cef5f535ccdcc223841cbf79c39542ce502e76
octoteo <octoclawos@outlook.com>
2026-09-16 11:19:58 +0800

    feat: add Android share-aware main activity

---
+++ native/android/MainActivity.java
@@ -0,0 +1,22 @@
+package com.octoteo.watersortsolver;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ShareReceiverPlugin.class);
+        super.onCreate(savedInstanceState);
+        ShareReceiverPlugin.captureIntent(this, getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        ShareReceiverPlugin.captureIntent(this, intent);
+    }
+}
